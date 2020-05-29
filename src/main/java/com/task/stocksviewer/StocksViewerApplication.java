ed5a96067d7aadc285f8e6e92792b8c13a10dec8
Komar, Jim <jim.komar@f-secure.com>
2020-05-29 13:43:21 +0300

    Add generated project code

---
+++ src/main/java/com/task/stocksviewer/StocksViewerApplication.java
@@ -0,0 +1,13 @@
+package com.task.stocksviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksViewerApplication.class, args);
+	}
+
+}
