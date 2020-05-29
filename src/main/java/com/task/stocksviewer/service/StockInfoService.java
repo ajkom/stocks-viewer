1996e2703ef08151e3b54b5c4e9ecae08387e5ef
Komar, Jim <jim.komar@f-secure.com>
2020-05-29 17:35:07 +0300

    Add initial service and class for csv data processing

---
+++ src/main/java/com/task/stocksviewer/service/StockInfoService.java
@@ -0,0 +1,37 @@
+package com.task.stocksviewer.service;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.task.stocksviewer.bean.StockInfo;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class StockInfoService {
+
+    private ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+    private String FILE_NAME = "DATA.csv";
+
+    public List<StockInfo> readCsvFile() throws URISyntaxException, IOException {
+
+        URL url = classLoader.getResource(FILE_NAME);
+        Reader reader = Files.newBufferedReader(Paths.get(url.toURI()));
+
+        CsvToBean<StockInfo> csvToBean = new CsvToBeanBuilder<StockInfo>(reader)
+                .withSeparator(',')
+                .withType(StockInfo.class)
+                .withSkipLines(1)
+                .build();
+
+        List<StockInfo> stocksList = null;
+
+        stocksList = csvToBean.parse();
+        return stocksList;
+
+    }
+}
